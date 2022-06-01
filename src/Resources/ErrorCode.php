a4206b4340629b5c04f9454bb3ffd5842152a4ed
hsul4n <7uthaifah.mustafa@gmail.com>
2022-06-01 11:49:58 +0300

    Handle insufficient query balance error

--- src/Resources/ErrorCode.php
+++ src/Resources/ErrorCode.php
@@ -6,12 +6,14 @@ class ErrorCode
 {
     const DUPLICATED_TRACKING_ID = "Error#6#Transaction ID Can not be Repeated";
     const SUBSCRIBER_EXCEED_RATE_LIMIT = "Error#9#هناك عملية سابقة لهذا الرقم لايمكن تكرار العملية قبل مرور 4 دقيقة/دقائق من وقت تنفيذ العملية السابقة";
+    const INSUFFICIENT_BALANCE_FOR_QUERY = "Error#8#رصيدكم لا يسمح باجراء عمليات إستعل";
 
     public static function all()
     {
         return [
             self::DUPLICATED_TRACKING_ID => 'Duplicated tracking id',
-            self::SUBSCRIBER_EXCEED_RATE_LIMIT => 'Sorry, the subscriber number has many payment please try again after 4 minuets'
+            self::SUBSCRIBER_EXCEED_RATE_LIMIT => 'Sorry, the subscriber number has many payment please try again after 4 minuets',
+            self::INSUFFICIENT_BALANCE_FOR_QUERY => 'Sorry, your balance is not enough to perform this operation',
         ];
     }
 
